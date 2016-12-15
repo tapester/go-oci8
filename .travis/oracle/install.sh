879c6b50ba45e892c94d690861c64c68b9bcc17a
tapester <future.shock@gmx.net>
2016-12-15 18:16:48 +0100

    Update install.sh

--- .travis/oracle/install.sh
+++ .travis/oracle/install.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -e
 
-ORACLE_RPM=`basename $ORACLE_FILE .zip`
-
 cd "$(dirname "$(readlink -f "$0")")"
 
 sudo apt-get -qq update
-sudo apt-get --no-install-recommends -qq install alien bc libaio1 unzip
+sudo apt-get --no-install-recommends -qq unzip
 
 df -B1 /dev/shm | awk 'END { if ($1 != "shmfs" && $1 != "tmpfs" || $2 < 2147483648) exit 1 }' ||
   ( sudo rm -r /dev/shm && sudo mkdir /dev/shm && sudo mount -t tmpfs shmfs -o size=2G /dev/shm )
@@ -15,4 +13,4 @@ test -f /sbin/chkconfig ||
 
 test -d /var/lock/subsys || sudo mkdir /var/lock/subsys
 
-unzip -j "$ORACLE_FILE" "*/$ORACLE_RPM"
+unzip -j "$ORACLE_FILE" "*/$ORACLE_FILE"
