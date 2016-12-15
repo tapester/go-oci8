9721ee88a206dc717658528256131f19756064a1
tapester <future.shock@gmx.net>
2016-12-15 18:27:50 +0100

    Update install.sh

--- .travis/oracle/install.sh
+++ .travis/oracle/install.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$(readlink -f "$0")")"
 
 sudo apt-get -qq update
-sudo apt-get --no-install-recommends -qq unzip
+sudo apt-get --no-install-recommends -qq install alien bc libaio1 unzip
 
 df -B1 /dev/shm | awk 'END { if ($1 != "shmfs" && $1 != "tmpfs" || $2 < 2147483648) exit 1 }' ||
   ( sudo rm -r /dev/shm && sudo mkdir /dev/shm && sudo mount -t tmpfs shmfs -o size=2G /dev/shm )
